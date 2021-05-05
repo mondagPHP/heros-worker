3c81dc49544990bf5b4c9c5b717d874d1ae130a9
chenzifan <chenzf@pvc123.com>
2021-05-05 10:26:00 +0800

    feat(master): remove error_function

--- src/http/UploadFile.php
+++ src/http/UploadFile.php
@@ -5,6 +5,7 @@
  * @contact  mondagroup_php@163.com
  *
  */
+
 namespace framework\http;
 
 use framework\exception\FileException;
@@ -74,21 +75,17 @@ class UploadFile extends \SplFileInfo
 
     /**
      * @param $destination
+     * @return \SplFileInfo
      * @throws \framework\exception\FileException
      */
     public function move($destination): \SplFileInfo
     {
-        set_error_handler(function ($type, $msg) use (&$error) {
-            $error = $msg;
-        });
         $path = pathinfo($destination, PATHINFO_DIRNAME);
-        if (! is_dir($path) && ! mkdir($path, 0777, true)) {
-            restore_error_handler();
-            throw new FileException(sprintf('Unable to create the "%s" directory (%s)', $path, strip_tags($error)));
+        if (!is_dir($path) && !mkdir($path, 0777, true)) {
+            throw new FileException(sprintf('Unable to create the "%s" directory', $path));
         }
-        if (! rename($this->getPathname(), $destination)) {
-            restore_error_handler();
-            throw new FileException(sprintf('Could not move the file "%s" to "%s" (%s)', $this->getPathname(), $destination, strip_tags($error)));
+        if (!rename($this->getPathname(), $destination)) {
+            throw new FileException(sprintf('Could not move the file "%s" to "%s"', $this->getPathname(), $destination));
         }
         restore_error_handler();
         @chmod($destination, 0666 & ~umask());

2743b3a7cf7e7cccf0fdfce281cd5db722d1b447
chenzifan <chenzf@pvc123.com>
2021-05-11 14:16:03 +0800

    feat(master): 增加session设置时间

--- src/bootstrap/Session.php
+++ src/bootstrap/Session.php
@@ -20,6 +20,9 @@ class Session implements Bootstrap
     public static function start(Worker $worker)
     {
         $config = config('session');
+        if (isset($config['gc_maxlifetime'])) {
+            ini_set('session.gc_maxlifetime', (int)$config['gc_maxlifetime']);
+        }
         if (isset($config['enable']) && $config['enable']) {
             Http::sessionName($config['session_name']);
             SessionBase::handlerClass($config['handler'], $config['config'][$config['handler']]);

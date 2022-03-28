b967337f235f01a78f30d77b12e796c6a6af498d
chenzifan <chenzf@pvc123.com>
2022-03-28 08:50:04 +0800

    add crontab by async task

---
+++ src/Crontab/AsyncTaskWorker.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+/**
+ * This file is part of monda-worker.
+ * @contact  mondagroup_php@163.com
+ */
+namespace Framework\Crontab;
+
+use Workerman\Connection\TcpConnection;
+
+/**
+ * 异步worker.
+ */
+class AsyncTaskWorker
+{
+    public function onMessage(TcpConnection $connection, string $data): void
+    {
+        $class = json_decode($data, true);
+        if (isset($class['clazz'], $class['method']) && class_exists($class['clazz']) && method_exists($class['clazz'], $class['method'])) {
+            call_user_func([new $class['clazz'](), $class['method']]);
+        }
+        $connection->send('ok');
+    }
+}
